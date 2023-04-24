80bf90ac51e17be26bc077c434e4f2d66a559da8
VatsalNagelia <vatsalnagelia17@gmail.com>
2023-04-24 23:37:57 +0530

    Add chakra-ui to enhance frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
+import {
+  ChakraProvider,
+  Box,
+  Heading,
+  VStack,
+  HStack,
+  FormControl,
+  Input,
+  Button,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+} from '@chakra-ui/react';
 const problems1 = [
   {
     title: "201. Bitwise AND of Numbers Range",
@@ -48,45 +65,92 @@ function App() {
   const [problems, setProblems] = useState([]);
 
   return (
-    <div>
-      <input type="text" placeholder="email"></input>
-      <input type="text" placeholder="password"></input>
-      <button>Sign In</button>
-      <div>
-        <button id="page1" onClick={() => {
-          setProblems(problems => problems1)
-        }}>Page 1</button>
-        <button id="page2" onClick={() => {
-          setProblems(problems => problems2)
-        }}>Page 2</button>
-      </div>
-      <div>
-        {problems.map(problem => <ProblemStatement
-          title={problem.title}
-          difficulty={problem.difficulty}
-          acceptanceRate={problem.acceptanceRate}
-        />)}
-      </div>
-    </div >
+    <ChakraProvider>
+      <Box minW="100vw" minH="100vh" p={4} bgColor="gray.100">
+        <Heading mb={6} textAlign="center" fontSize={["2xl", "3xl", "4xl"]}>
+          Problem Solving Platform
+        </Heading>
+
+        <VStack
+          align="center"
+          spacing={6}
+          maxW="lg"
+          mx="auto"
+          p={6}
+          bgColor="white"
+          borderRadius="md"
+          boxShadow="lg"
+        >
+          <HStack w="100%">
+            <FormControl>
+              <Input placeholder="Email" type="email" />
+            </FormControl>
+            <FormControl>
+              <Input placeholder="Password" type="password" />
+            </FormControl>
+            <Button colorScheme="blue">Sign In</Button>
+          </HStack>
+          <HStack spacing={4}>
+            <Button
+              colorScheme="teal"
+              onClick={() => {
+                setProblems(() => problems1);
+              }}
+            >
+              Page 1
+            </Button>
+            <Button
+              colorScheme="teal"
+              onClick={() => {
+                setProblems(() => problems2);
+              }}
+            >
+              Page 2
+            </Button>
+          </HStack>
+          <Table variant="striped" colorScheme="teal">
+            <Thead>
+              <Tr>
+                <Th>Title</Th>
+                <Th>Difficulty</Th>
+                <Th>Acceptance Rate</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {problems.map((problem, index) => (
+                <ProblemStatement
+                  key={index}
+                  title={problem.title}
+                  difficulty={problem.difficulty}
+                  acceptanceRate={problem.acceptanceRate}
+                />
+              ))}
+            </Tbody>
+          </Table>
+        </VStack>
+      </Box>
+    </ChakraProvider>
   )
 }
 
 function ProblemStatement(props) {
-  const title = props.title;
-  const difficulty = props.difficulty;
-  const acceptanceRate = props.acceptanceRate;
-
+  const { title, difficulty, acceptanceRate } = props;
 
   return (
-    <tr>
-      <td>{title}</td>
-      <td style={{ color: difficulty === "Medium" ? "yellow" : "red" }}>
+    <Tr>
+      <Td>{title}</Td>
+      <Td color={difficulty === "Medium" ? "yellow.400" : "red.500"}>
         {difficulty}
-      </td>
-      <td>
-        {acceptanceRate}
-      </td>
-    </tr>)
+      </Td>
+      <Td>{acceptanceRate}</Td>
+    </Tr>
+  );
 }
+ProblemStatement.propTypes = {
+  title: PropTypes.string.isRequired,
+  difficulty: PropTypes.string.isRequired,
+  acceptanceRate: PropTypes.string.isRequired,
+};
+
 
 export default App
